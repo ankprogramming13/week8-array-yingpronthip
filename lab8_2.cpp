1c2e88ac385474a3954faf9e12f236894b7ac1d8
Unknown <viratagift@gmail.com>
2018-01-19 14:41:36 +0700

    ข้อที่ 2

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -4,14 +4,26 @@ using namespace std;
 
 int main () {
 
-        char name[] = {"Anukul"};
-        int nameLength = 6;
-        cout << name;
+        char name[] = {"Sutamas"};
+        int nameLength = 0;
+        cout << name << endl;
 
         // TODO: เขียนโปรแกรมเพื่อสลับชื่อ
         // hint : สร้าง array มารับค่าตัวอักษรแบบย้อนกลับ สามารถใช้ loop เข้ามาช่วยได้
+        char christ[8];
+
+        // เขียนโปรแกรมเพื่อแสดงค่าใน arr
+
+    // do loop execution
+        for(nameLength = 0; nameLength < 7; nameLength++) {
+            christ[nameLength] = name [6-nameLength]+1;
+        }
+        christ[7] = '\0';
+
+        cout << christ << endl;
 
 
-
-        return 0;
-}
+    return 0;
+  }
+Nipaporn Nakcharoen
+คนคล้ำ108ท่า
